07d0f7088d6d997ae5f927478f0b72edb7ff7542
wl <1686201650@qq.com>
2022-07-28 20:14:17 +0800

    hot-fix  commit1

--- src/main/java/cn/wl/GitTest.java
+++ src/main/java/cn/wl/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello git!");
         System.out.println("hello wl!");
         System.out.println("hello wl1!");
+        System.out.println("hello.txt");
     }
 }
